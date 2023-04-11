e196d5fbe1cf70c6d1c0c2d8b639cf00e67d4b85
Alexei Valchuk <a.valchukav@gmail.com>
2023-04-11 20:48:27 +0300

    feat: provide initial settings

---
+++ src/test/java/ru/avalc/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.avalc.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
